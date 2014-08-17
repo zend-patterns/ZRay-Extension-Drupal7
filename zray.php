af1bbc962f846814d49c21fd4b1209edf65f2548
yonni <yonni.m@zend.com>
2014-08-17 14:02:35 +0300

    Added called user functions tracking Added user properties storage

--- zray.php
+++ zray.php
@@ -16,6 +16,38 @@ $zre->traceFunction('drupal_load', function(){}, function($context, & $storage){
 	$storage['LoadedModules'] = array('module' => $module);
 });
 
+$zre->traceFunction('call_user_func', function(){}, function($context, & $storage){
+	$called = $context['functionArgs'][0];
+	$parameter = isset($context['functionArgs'][1]) ? $context['functionArgs'][1] : '';
+	$blob = isset($context['functionArgs'][2]) ? json_encode($context['functionArgs'][2]) : '';
+	
+ 	$storage['CalledFunctions'] = array('called' => $called, 'parameter' => $parameter, 'info' => $blob);
+});
+
+$zre->traceFunction('menu_execute_active_handler', function(){}, function($context, & $storage) {
+	global $user;
+	
+	$stateUser = (array)$user;
+	$stateUserKeys = array_keys($stateUser);
+	
+	$stateUser['roles'] = array_reduce($stateUser['roles'], function($reduced, $item){
+		if ($reduced == '') {
+			$reduced = $item;
+		} else {
+			$reduced .= ", $item";
+		}
+		return $reduced;
+	});
+	
+	$stateUserValues = array_map(function($key) use ($stateUser) {
+		return array('property' => $key, 'value' => $stateUser[$key]);
+	}, $stateUserKeys);
+	
+	
+	
+	$storage['userProperties'] = $stateUserValues;
+});
+
 $zre->traceFunction('drupal_retrieve_form', function(){}, function($context, & $storage) {
 
 	$formName = $context['functionArgs'][0];
@@ -40,3 +72,4 @@ $zre->traceFunction('drupal_retrieve_form', function(){}, function($context, & $
  			'cache' => $cache, 'method' => $method, 'activity' => $activity
  	);
 });
+
