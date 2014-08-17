a7b349b76f61635a4e0d8f541350799ff4fb25a1
yonni <yonni.m@zend.com>
2014-08-17 12:07:44 +0300

    Added hooks for module_invoke, drupal_load and retrieve_form

--- zray.php
+++ zray.php
@@ -1,3 +1,42 @@
 <?php
 
 $zre = new \ZRayExtension('drupal');
+
+$zre->traceFunction('module_invoke', function(){}, function($context, & $storage){
+	$module = $context['functionArgs'][0];
+	$action = $context['functionArgs'][1];
+	$hook = isset($context['functionArgs'][2]) ? $context['functionArgs'][2] : '';
+	
+	$storage['moduleInvoke'] = array('module' => $module, 'action' => $action, 'hook' => $hook);
+});
+
+$zre->traceFunction('drupal_load', function(){}, function($context, & $storage){
+ 	$module = $context['functionArgs'][1];
+	
+	$storage['LoadedModules'] = array('module' => $module);
+});
+
+$zre->traceFunction('drupal_retrieve_form', function(){}, function($context, & $storage) {
+
+	$formName = $context['functionArgs'][0];
+ 	$formId = $context['functionArgs'][1]['build_info']['form_id'];
+ 	$baseFormId = $context['functionArgs'][1]['build_info']['base_form_id'];
+ 	$cache = $context['functionArgs'][1]['cache'];
+ 	$method = $context['functionArgs'][1]['method'];
+ 	$activity = 'Display';
+ 	
+ 	if ($context['functionArgs'][1]['executed']) {
+ 		$activity = 'Executed';
+ 	} elseif ($context['functionArgs'][1]['programmed']) {
+ 		$activity = 'Programmed';
+ 	} elseif ($context['functionArgs'][1]['rebuild']) {
+ 		$activity = 'Rebuilt';
+ 	} elseif ($context['functionArgs'][1]['rebuild']) {
+ 		$activity = 'Rebuilt';
+ 	}
+ 	
+ 	$storage['RetrievedForms'] = array(
+ 			'formName' => $formName, 'formId' => $formId, 'baseFormId' => $baseFormId,
+ 			'cache' => $cache, 'method' => $method, 'activity' => $activity
+ 	);
+});
